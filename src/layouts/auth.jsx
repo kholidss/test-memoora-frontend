a84543fd78e9532e2944041b5e70e8476b4c086b
kholid saifulloh <muhammadkholid@refactory.id>
2021-11-14 15:00:11 +0700

    add react hooks form with yup validation

--- src/layouts/auth.jsx
+++ src/layouts/auth.jsx
@@ -4,9 +4,9 @@ import Icon from '../components/icons'
 
 const Auth = function () {
   return (
-    <div className="lg:flex xl:max-w-screen-sm overflow-hidden">
+    <div className="lg:flex">
       <div className="lg:w-1/2 xl:max-w-screen-sm">
-        <div className="py-5 bg-gray-200 flex justify-center lg:justify-start lg:px-12">
+        <div className="py-8 bg-gray-100 flex justify-center lg:justify-start lg:px-12">
           <Link to="/" className="cursor-pointer flex items-center">
             <div>
               <Icon className="w-10 text-blue-800" name="logo" size={28} />
@@ -14,11 +14,11 @@ const Auth = function () {
             <div className="text-3xl text-blue-800 tracking-wide ml-1 font-semibold">Memoora</div>
           </Link>
         </div>
-        <div className="mt-14 px-12 sm:px-24 md:px-48 lg:px-12 pb-20 lg:mt-10 xl:px-24 xl:max-w-2xl">
+        <div className="mt-14 px-12 sm:px-24 md:px-48 lg:px-12 pb-20 lg:mt-16 xl:px-24 xl:max-w-2xl">
           <Outlet />
         </div>
       </div>
-      <div className="hidden lg:flex items-center justify-center bg-blue-100 flex-1 h-screen ">
+      <div className="hidden lg:flex items-center justify-center bg-blue-100 flex-1 h-screen">
         <div className="max-w-xs transform duration-200 hover:scale-110 cursor-pointer">
           <img
             lass="w-5/6 mx-auto"
